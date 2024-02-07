94c582484970c688a98074700f0338f029f4b4bd
dgilles5 <dsg25225@email.vccs.edu>
2024-02-07 14:53:05 -0500

    Wrote initial program

--- FirstCProject/FirstCProject.cpp
+++ FirstCProject/FirstCProject.cpp
@@ -2,11 +2,26 @@
 //
 
 #include <iostream>
+using namespace std;
+#include <fstream>
+#include <string>
 
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+
+    double input_price;
+    double sales_tax = 0.0625;
+    double mark_up_percentage = 0.6;
+    double final_price;
+
+    std::cout << "Please input the price of your item: ";
+    cin >> input_price;
+
+    cout << "Your input price was:$" << input_price << endl;
+    final_price = (input_price + (input_price * mark_up_percentage)) * sales_tax + input_price;
+    cout << "The mark up percentage is:$" << (mark_up_percentage * 100) << "%" << endl;
+    cout << "The sales tax is:" << (sales_tax * 100) << "%" << endl;
+    cout << "The final price is:$" << final_price << endl;
     return 0;
 }
 
