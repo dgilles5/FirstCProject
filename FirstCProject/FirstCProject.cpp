b16750342b048db68cbb6b052c9391e91330661a
dgilles5 <dsg25225@email.vccs.edu>
2024-02-07 14:59:18 -0500

    fixed the percents and $ signs fixed logic.

--- FirstCProject/FirstCProject.cpp
+++ FirstCProject/FirstCProject.cpp
@@ -3,25 +3,26 @@
 
 #include <iostream>
 using namespace std;
-#include <fstream>
+#include <iomanip>
 #include <string>
 
 
 int main() {
 
     double input_price;
-    double sales_tax = 0.0625;
+    double sales_tax = 0.0525;
     double mark_up_percentage = 0.6;
     double final_price;
 
     std::cout << "Please input the price of your item: ";
     cin >> input_price;
+    double markup_price = (input_price * mark_up_percentage) + input_price;
 
-    cout << "Your input price was:$" << input_price << endl;
-    final_price = (input_price + (input_price * mark_up_percentage)) * sales_tax + input_price;
-    cout << "The mark up percentage is:$" << (mark_up_percentage * 100) << "%" << endl;
-    cout << "The sales tax is:" << (sales_tax * 100) << "%" << endl;
-    cout << "The final price is:$" << final_price << endl;
+    cout << "Your input price was: $" << input_price << endl;
+    final_price = markup_price * sales_tax + markup_price;
+    cout << "The mark up percentage is: " << (mark_up_percentage * 100) << "%" << endl;
+    cout << "The sales tax is: " << (sales_tax * 100) << "%" << endl;
+    cout << "The final price is: $" << final_price << fixed << setprecision(2) << endl;
     return 0;
 }
 
